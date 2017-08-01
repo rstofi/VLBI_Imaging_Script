49c051d13b25d2a5e3f1cf32ad0656e8525f6207
Kristof Rozgonyi <rstofi@gmail.com>
2017-08-02 01:27:47 +0200

    second test commit message

---
+++ VLBI_imaging_functions.py
@@ -0,0 +1,7 @@
+import os; #for terminal commands
+import numpy as np; #numeric python
+import math; #for negative power function
+import subprocess; #for running difmap & gnuplot
+import time; #for time measuring
+from IPython.display import Image; #for image displaying 
+import pylab; #for data reading
