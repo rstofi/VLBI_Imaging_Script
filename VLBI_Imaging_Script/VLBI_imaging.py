21859252b0724c2db375d2b31a97e8a5fe92da32
Kristof Rozgonyi <rstofi@gmail.com>
2017-08-02 03:12:55 +0200

    modelfit script tested

--- VLBI_Imaging_Script/VLBI_imaging.py
+++ VLBI_Imaging_Script/VLBI_imaging.py
@@ -1,5 +1,32 @@
 from VLBI_imaging_functions import *;
 
-difmap_imaging_script(visibility_file='J0017+8135_S_1998_10_01_pus_vis.fits',object_name='J0017+8135_S_1998_10_01',clean_sigma=6,map_size=512,pixel_size=0.3,observation_length=900,script_name='test_run');
+### Imaging script ###
+'''
+difmap_imaging_script(visibility_file='J0017+8135_S_1998_10_01_pus_vis.fits',
+						output_name='J0017+8135_S_1998_10_01',
+						clean_sigma=6,
+						map_size=512,
+						pixel_size=0.3,
+						observation_length=900,
+						script_name='test_run');
 
 run_difmap_imaging_script(script_name='test_run');
+'''
+
+### Modelling script ###
+difmap_modeling_script(visibility_file='J0017+8135_S_1998_10_01_pus_vis.fits',
+						output_name='J0017+8135_S_1998_10_01',
+						clean_sigma=6,
+						map_size=512,
+						pixel_size=0.3,
+						observation_length=900,
+						model_sigma=6,
+						phi=0,
+						major_axis=0.02,
+						minor_axis=1,
+						model_type=1,
+						model_iter=10,
+						max_jet_component_number=2,
+						script_name='model_test_run');
+
+run_difmap_imaging_script(script_name='model_test_run');
--- VLBI_Imaging_Script/VLBI_imaging_functions.py
+++ VLBI_Imaging_Script/VLBI_imaging_functions.py
@@ -4,7 +4,13 @@ import math; #for negative power function
 import subprocess; #for running difmap on Linux
 import pylab; #for data reading
 
-def difmap_imaging_script(visibility_file,object_name,clean_sigma,map_size,pixel_size,observation_length,**kwargs):
+def difmap_imaging_script(visibility_file,
+							output_name,
+							clean_sigma,
+							map_size,
+							pixel_size,
+							observation_length,
+							**kwargs):
 	"""
 	This function creates the difmap imaging .log script
 	"""
@@ -122,11 +128,201 @@ save clean_map_{obj}
 
 !quit from difmap
 quit'''.format(obj_file=visibility_file,
-				obj=object_name,
+				obj=output_name,
 				clean_sigma=clean_sigma,
 				map_size=map_size,
 				pixel_size=pixel_size,
 				observation_length=observation_length));
+
+	fn.close();
+
+def difmap_modeling_script(visibility_file,
+							output_name,
+							clean_sigma,
+							map_size,
+							pixel_size,
+							observation_length,
+							model_sigma,
+							phi,
+							major_axis,
+							minor_axis,
+							model_type,
+							model_iter,
+							max_jet_component_number,
+							**kwargs):
+	"""
+	This function creates the difmap modeling .log script
+	"""
+	
+	
+	if 'script_name' in kwargs:
+		fn = open('%s.log' %kwargs['script_name'], 'a');
+	else:	
+		fn = open("difmap_modeling_script.log", "a");
+
+	fn.write('''!set imaging pypeline parameters
+!declare variables for imaging
+
+float clean_sigma; clean_sigma  = {clean_sigma}
+float map_size; map_size = {map_size}
+float pixel_size; pixel_size = {pixel_size}
+float observation_length; observation_length = {observation_length}
+
+float signal_to_noise_p;
+float signal_to_noise_a;
+
+!declare variables for model fitting
+
+float model_sigma; model_sigma = {model_sigma};
+
+float phi; phi = {phi};
+float major_axis; major_axis = {major_axis};
+float minor_axis; minor_axis = {minor_axis};
+float m_type; m_type = {m_type};
+float model_iter; model_iter = {model_iter};
+	
+!Imaging
+
+observe {obj_file}
+select i
+mapsize map_size,pixel_size
+
+print "HEADER"
+header
+print "END_OF_HEADER"
+
+startmod "",1
+uvw 2,0
+peakwin 1.5
+clean 100,0.05
+selfcal false,false,0
+
+!if snr > clean_sigma more calibration steps will be done
+
+!clean-selfcal calibration:
+if(peak(flux,max)/imstat(rms) > clean_sigma)
+	repeat;\
+		peakwin 1.5; clean; selfcal
+	until(peak(flux,max)/imstat(rms) < clean_sigma)
+end if
+
+uvw 2,-1
+
+!clean-selfcal calibration:
+if(peak(flux,max)/imstat(rms) > clean_sigma)
+	repeat;\
+		peakwin 1.5; clean; selfcal
+	until(peak(flux,max)/imstat(rms) < clean_sigma)
+end if
+
+uvw 0,-1
+
+!clean-selfcal calibration:
+if(peak(flux,max)/imstat(rms) > clean_sigma)
+	repeat;\
+		peakwin 1.5; clean; selfcal
+	until(peak(flux,max)/imstat(rms) < clean_sigma)
+end if
+
+gscale
+
+!here one should use tlpot to determine the true value of actual_time
+
+!observation time domain calibration:
+signal_to_noise_p = peak(flux,max)/imstat(rms)
+repeat;\
+	selfcal true,true,observation_length
+	signal_to_noise_a = peak(flux,max)/imstat(rms)
+	if(peak(flux,max)/imstat(rms) > clean_sigma)
+		if(signal_to_noise_a <= signal_to_noise_p)
+			peakwin 1.5; clean; selfcal
+		observation_length=observation_length/2
+			signal_to_noise_a = signal_to_noise_p
+		end if
+		if(signal_to_noise_a > signal_to_noise_p)
+			clrmod false,true
+		observation_length=observation_length/2
+			signal_to_noise_a = signal_to_noise_p
+		end if
+	else
+		observation_length=observation_length/2
+	end if
+until(observation_length < 2)
+
+selfcal true,true,0
+
+delwin
+clean 1000,0.01
+device /NULL
+mapl
+cmul=3*imstat(rms)
+
+!Save image
+!device {obj}.ps/vcps
+!device {obj}.ps/vps
+!mapl clean, false 
+
+!get clean image and beam statistics
+print "MARKING_STRING"
+print peak(flux)
+print imstat(rms)
+print cmul
+print imstat(bmin)
+print imstat(bmaj)
+print imstat(bpa)
+print "END_MARKING"
+
+!Modeling
+
+clrmod true
+
+!Print flux
+print "FLUX_MARKING"
+print imstat(rms)
+print peak(flux)
+print "END_FLUX_MARKING"
+
+!Fit first (elliptical-gaussian) component
+
+addcmp peak(flux),true,peak(x),peak(y),true,{major_axis},true,{minor_axis},true,{phi},true,{m_type}
+modelfit model_iter
+
+!Fit other (cirle-gaussian) components
+i = 0
+repeat;\
+	i = i + 1
+	if(peak(flux)/imstat(rms) > model_sigma)
+		addcmp peak(flux),true,peak(x),peak(y),true,1,false,1,false,{phi},true,1
+		modelfit model_iter
+	end if
+until(i >= {max_loop})
+
+
+!Image with model components
+!device {obj}.ps/vcps
+device {obj}.ps/vps
+mapl clean, true 
+
+!Write model components parameters to a .mod file
+wmod {obj}.mod
+
+!save modelfit map
+save model_map_{obj}
+
+!quit from difmap
+quit'''.format(obj_file=visibility_file,
+				obj=output_name,
+				clean_sigma=clean_sigma,
+				map_size=map_size,
+				pixel_size=pixel_size,
+				observation_length=observation_length,
+				model_sigma=model_sigma,
+				phi=phi,
+				major_axis=major_axis,
+				minor_axis=minor_axis,
+				m_type=model_type,
+				model_iter=model_iter,
+				max_loop=max_jet_component_number));
 
 	fn.close();
 
